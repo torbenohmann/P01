c37bf6252030bc2c4dc853112da072ad37cf6050
Torben Ohmann <ohmann.torben@gmail.com>
2026-04-29 21:39:05 +0200

    A2_2

--- V2.cpp
+++ V2.cpp
@@ -2,7 +2,7 @@
 #include "Pathfinder.h"
 #include <iostream>
 #include <fstream>
-
+#include <string>
 std::vector<std::vector<GridTile>> map;
 
 int importMap_Vorbereitung(std::string path){
@@ -12,7 +12,7 @@ int importMap_Vorbereitung(std::string path){
     map.clear();
     
     std::string line;
-    while (getline(csvFile, line)) {
+    while (std::getline(csvFile, line)) {
         map.push_back({});
         for(char& c : line){
             if(c == '*'){
