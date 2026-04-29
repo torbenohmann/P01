5237fd537a9ee90c07d59ff1d4ec91ff9329b1ff
Torben Ohmann <ohmann.torben@gmail.com>
2026-04-29 21:46:58 +0200

    A4_1

--- CGridtypeMap.h
+++ CGridtypeMap.h
@@ -48,7 +48,7 @@ class CGridtypeMap{
         }
 
         void toConsole(bool displayCost){
-            system("clear");
+            system("cls");
             for(auto& line : map){
                 for(auto& tile : line){
                     if(tile.type == TileType::obstacle) cout << "#";
